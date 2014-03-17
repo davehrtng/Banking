e4c187060994dea5482f793ea04a731be03c0813
David Harting <dave.hrtng@gmail.com>
2014-03-17 08:37:51 -0400

    adds test proj and entities

---
+++ Banking/Entity/Transaction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    public class Transaction
+    {
+        public enum Type { credit, debit }; 
+        
+        private decimal amount;
+        private DateTime date;
+        private Type type;
+
+        public Transaction() { }
+
+        public Transaction(decimal amt, DateTime dt, Type typ)
+        {
+            amount = Math.Abs(amt);
+            date = dt;
+            type = typ; 
+        }
+
+        public decimal Amount
+        {
+            get { return amount; }
+            set { amount = Math.Abs(value); }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public Type CreditOrDebit
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        
+    }
+}
---
+++ BankingTest/TransactionTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Banking; 
+
+namespace BankingTest
+{
+    [TestClass]
+    public class TransactionTest
+    {
+        [TestMethod]
+        public void transaction_get()
+        {
+            Transaction transaction = new Transaction(35.00m, new DateTime(2014, 3, 7), Transaction.Type.credit);
+            decimal amt = transaction.Amount;
+            Transaction.Type type = transaction.CreditOrDebit;
+            DateTime date = transaction.Date; 
+        }
+
+        [TestMethod]
+        public void transaction_set()
+        {
+            Transaction transaction = new Transaction();
+            transaction.Amount = 35.00m;
+            transaction.CreditOrDebit = Transaction.Type.debit;
+            transaction.Date = DateTime.Today; 
+        }
+
+        [TestMethod]
+        public void transaction_negative_amount()
+        {
+            Transaction trans = new Transaction(-40.00m, new DateTime(1992, 3, 4), Transaction.Type.debit);
+            decimal amt_t1 = trans.Amount;
+            trans.Amount = -400.00m; 
+            decimal amt_t2 = trans.Amount; 
+        }
+    }
+}
